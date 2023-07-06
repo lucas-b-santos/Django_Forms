c9e0d96a56ee5fd24af3cba5568dd0854c73d904
Lucas Batista dos Santos <batistalucas451@gmail.com>
2023-07-06 00:16:53 -0300

    Estabelecendo funcoes de  limpeza para os campos

--- formulario/forms.py
+++ formulario/forms.py
@@ -1,28 +1,63 @@
 from django import forms
-from .models import Dados
+from .models import Proprietario
 from django.forms.widgets import NumberInput
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+import datetime
 
-placeholders = ['nome',  'email']
-
-class formPrincipal(forms.ModelForm):
-
-
+class formProprietario(forms.ModelForm):
     # Esse construtor eu uso para passar atributos do HTML para os inputs do form gerado
     #O atributo type não dá pra alterar aqui, altero via widgets
     def __init__(self, *args, **kwargs):
-        super(formPrincipal, self).__init__(*args, **kwargs)
+        super(formProprietario, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'}) #<- dicionario com cada atributo e seu respectivo valor desejado
-            if field in placeholders:
-                self.fields[field].widget.attrs.update({'placeholder': field.capitalize()})
-
 
     class Meta:
-        model = Dados
+        model = Proprietario
         fields = "__all__"
         widgets = {
-            'data_nascimento':NumberInput(attrs={'type': 'date'}),
+            'data_nascimento':NumberInput(attrs={'type': 'date','max': datetime.date.today()}),
         }
+    
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+    def clean_nome(self):
+        data = self.cleaned_data['nome']
+        return data
+
+    def clean_rg(self):
+        data = self.cleaned_data['rg']
+        return data
+
+    def clean_cpf_cnpj(self):
+        data = self.cleaned_data['cpf_cnpj']
+        return data
+
+    def clean_telefone(self):
+        data = self.cleaned_data['telefone']
+        return data
+    
+    def clean_data_nascimento(self):
+        data = self.cleaned_data['data_nascimento']
+        return data
+    
+    def clean_cidade(self):
+        data = self.cleaned_data['cidade']
+        return data
+    
+    def clean_estado(self):
+        data = self.cleaned_data['estado']
+        return data
+    
+    def clean_pais(self):
+        data = self.cleaned_data['pais']
+        return data
+
+
+  
         
         
        
--- formulario/views.py
+++ formulario/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import formPrincipal
+from .forms import formProprietario
 
 
 # Create your views here.
 def teste(request): 
     if request.method == 'GET':
-        form = formPrincipal
+        form = formProprietario
         context = {"form": form}
         return render(request, 'form.html', context)
     else:
-        form = formPrincipal(request.POST)
+        form = formProprietario(request.POST)
         if form.is_valid():
             form.save()
-        redirect('/')
+            form = formProprietario
+        context = {"form": form}
+        return render(request, 'form.html', context) 
+        
+
+ 
+
+   
+        
